7b27ec9cb5cd3a5e7d23c84da8f563b055923b50
unknown <abello10@outlook.com>
2025-11-26 23:14:09 -0300

    hola

--- src/main/java/com/proyecto/groove/config/WebConfig.java
+++ src/main/java/com/proyecto/groove/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://groove-store.vercel.app") 
+                        .allowedOrigins("http://localhost:5173", "https://frontend-groove.vercel.app") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*");
             }
